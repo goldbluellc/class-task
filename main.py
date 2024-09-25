829a1649039056a7dcfbaf078ac341970d84ce2d
goldbluellc <119589981+goldbluellc@users.noreply.github.com>
2024-09-25 13:05:16 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator!\n")
+
+city_name = input("What is the city that you grew up in?\n")
+
+pet_name = input("What is the name of your pet?\n")
+
+band_name = city_name + " " + pet_name
+
+print("\nYour band name is: " + band_name)
